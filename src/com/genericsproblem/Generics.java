4527a37b06d2a976b0335f0afa7d4ba971a92287
sumi <sumipavib1302@gmail.com>
2023-01-31 19:08:28 +0530

    Added:Given 3 Integers find maximum

--- src/com/genericsproblem/Generics.java
+++ src/com/genericsproblem/Generics.java
@@ -2,23 +2,23 @@ package com.genericsproblem;
 
 public class  Generics {
 	
-	static void max_variables(Float num1, Float num2, Float num3){
+	static void max_variables(Integer num1, Integer num2, Integer num3){
         if(num1.compareTo(num2) > 0){
             if(num1.compareTo(num3) > 0){
-                System.out.println( num1 + " is Maximum of three float numbers ");
+                System.out.println( num1 + " is Maximum of three numbers ");
             }else {
-                System.out.println( num3 + " is Maximum of three float numbers ");
+                System.out.println( num3 + " is Maximum of three numbers ");
             }
         }else{
             if(num2.compareTo(num3) > 0){
-                System.out.println( num2 + " is Maximum of three float numbers ");
+                System.out.println( num2 + " is Maximum of three numbers ");
             }else{
-                System.out.println( num3 + " is Maximum of three float numbers ");
+                System.out.println( num3 + " is Maximum of three numbers ");
             }
         }
     }
 
     public static void main(String[] args) {
-        max_variables(1.3f,1.5f,0.2f);
+        max_variables(345,445,1000);
     }
 }
